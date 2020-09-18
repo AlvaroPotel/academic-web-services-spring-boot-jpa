bbfa9e32486cc21faf429c31198937c15e710cc0
AlvaroPotel <alvaropotel@gmail.com>
2020-09-18 15:47:37 +0100

    Service layer, component registration

---
+++ src/main/java/com/example/academicspringjpa/services/UserServices.java
@@ -0,0 +1,27 @@
+package com.example.academicspringjpa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.academicspringjpa.entities.User;
+import com.example.academicspringjpa.repositories.UserRepository;
+
+@Service
+public class UserServices {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
